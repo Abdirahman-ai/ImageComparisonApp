468a778858b7d514c7068009541fe2db9b0fb53b
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2023-08-02 17:26:33 -0500

    documented the code

--- ImageDiff.py
+++ ImageDiff.py
@@ -1,17 +1,27 @@
 import sys
+# Prints out the location of the Virtual Environment
+# To run the program correctly, navigate to this location
+# then activate it with the following commands
+# source myenv/bin/activate
+print(sys.executable)
+
 import cv2
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 from skimage.metrics import structural_similarity as ssim
- 
+
+# A function to resize two images while maintaining their aspect ratio
 def resize_images(image1, image2, new_size):
-    # Maintain the aspect ratio while resizing
+    # Get the dimensions of the images
     h1, w1 = image1.shape[:2]
     h2, w2 = image2.shape[:2]
+
+    # Calculate the aspect ratios
     aspect_ratio1 = w1 / h1
     aspect_ratio2 = w2 / h2
 
+    # Determine the new dimensions while maintaining the aspect ratio
     if aspect_ratio1 > aspect_ratio2:
         new_width = int(new_size * aspect_ratio1)
         new_height = new_size
@@ -19,16 +29,19 @@ def resize_images(image1, image2, new_size):
         new_width = new_size
         new_height = int(new_size / aspect_ratio2)
 
+    # Resize the images
     image1 = cv2.resize(image1, (new_width, new_height))
     image2 = cv2.resize(image2, (new_width, new_height))
     return image1, image2
 
+# A function to open an image file and return the image data
 def load_image():
     file_path = filedialog.askopenfilename()
     if file_path:
         image = cv2.imread(file_path)
         return image
 
+# A function to compare two images using Structural Similarity Index (SSIM)
 def compare_images(image1, image2):
     gray_image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     gray_image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
@@ -37,35 +50,39 @@ def compare_images(image1, image2):
 
 # ... (previous code)
 
+# A function to update the displayed images and comparison results
 def update_images():
+    # Ask the user to select two image files
     file_path1 = filedialog.askopenfilename()
     file_path2 = filedialog.askopenfilename()
 
     if file_path1 and file_path2:
+        # Load the selected images
         image1 = cv2.imread(file_path1)
         image2 = cv2.imread(file_path2)
 
-        # Calculate the new size based on the window size
+        # Calculate the maximum size that fits within the application window
         window_width = root.winfo_width()
         window_height = root.winfo_height()
-        new_size = min(window_width, window_height) - 50
+        max_size = min(window_width // 2, window_height - 100)
 
-        # Optionally, resize the images (uncomment the next line if needed)
-        image1, image2 = resize_images(image1, image2, new_size)
+        # Resize the images to fit the maximum size while maintaining their aspect ratio
+        image1, image2 = resize_images(image1, image2, max_size)
 
+        # Compare the images and get the SSIM score and difference image
         ssim_score, diff_image = compare_images(image1, image2)
 
         # Display the SSIM score and result
         result_label.config(text=f"SSIM Score: {ssim_score:.4f}")
 
-        # Display the similarity message
+        # Display the similarity message based on the SSIM score
         if ssim_score == 1.0:
             message = "The images are identical."
         elif ssim_score > 0.8:
             message = "The images are very similar."
         else:
             message = "The images have differences."
-        message_label.config(text=message)
+        message_label2.config(text=message)
 
         # Clear the diff_label before displaying the difference image
         diff_label.config(image=None)
@@ -79,7 +96,7 @@ def update_images():
         diff_image_tk = ImageTk.PhotoImage(diff_image_pil)
 
         # Update diff_label with the new difference image and resize it
-        diff_label.config(image=diff_image_tk, width=new_size, height=new_size)
+        diff_label.config(image=diff_image_tk, width=max_size, height=max_size)
         diff_label.image = diff_image_tk  # Keep a reference to prevent garbage collection
 
         # Display the original images side by side
@@ -95,7 +112,6 @@ def update_images():
         image_label2.image = image2_tk  # Keep a reference to prevent garbage collection
 
 # ... (rest of the code)
-
 
 # Create the Tkinter application and the GUI layout
 root = tk.Tk()
@@ -115,7 +131,9 @@ description_text += "closer to 0 indicate more dissimilarity. Images that are vi
 message_label = tk.Label(root, text=description_text, font=("Arial", 12), justify='left', anchor='w')
 message_label.grid(row=1, column=0, columnspan=2, padx=30, pady=10)
 
-# Create buttons and labels
+# ... (previous code)
+
+# Create buttons and labels for the image comparison
 result_label = tk.Label(root, text="", font=("Arial", 12, "bold"))
 message_label2 = tk.Label(root, text="", font=("Arial", 12, "bold"))
 diff_label = tk.Label(root)
@@ -124,11 +142,11 @@ image_label2 = tk.Label(root)
 load_button = tk.Button(root, text="Load Images", command=update_images)
 
 # Grid layout for other widgets
-result_label.grid(row=2, column=0, columnspan=2, padx=40, pady=10)
-message_label2.grid(row=3, column=0, columnspan=2, padx=40, pady=10)
+result_label.grid(row=2, column=0, columnspan=40, padx=40, pady=10)
+message_label2.grid(row=3, column=0, columnspan=40, padx=40, pady=10)
 image_label1.grid(row=4, column=0, padx=40, pady=10)
 image_label2.grid(row=4, column=1, padx=40, pady=10)
-diff_label.grid(row=5, column=0, columnspan=2, padx=40, pady=10)
-load_button.grid(row=6, column=0, columnspan=2, padx=40, pady=20)
+diff_label.grid(row=5, column=0, columnspan=40, padx=40, pady=10)
+load_button.grid(row=6, column=0, columnspan=40, padx=40, pady=20)
 
 root.mainloop()
